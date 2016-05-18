c9e68c7acad48cf431b496b4b2a091aef7018dc9
vica86 <victoria.galanciuc@gmail.com>
2016-05-18 14:26:24 +0300

    Update super program v.0.0.1

--- SDADOOM.c
+++ SDADOOM.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     FILE* cross_road_file_pointer;
     cross_road_file_pointer = fopen("Crossroad.txt","r");
     char Array_of_cross_roads[152][500];
+    char Name_of_cross_roads[152][200];
     char Cross_Road[500];
-    int i = 0;
+    char *token;
+    char name[200];
+    int i = 0,contor;
     while(!feof(cross_road_file_pointer)){
         fgets(Cross_Road, 500, cross_road_file_pointer);
         strcpy(Array_of_cross_roads[i],Cross_Road);
@@ -13,6 +17,14 @@ int main(){
     };
     fclose(cross_road_file_pointer);
     for(i = 0; i < 3; i++){
-        printf("%s",Array_of_cross_roads[i]);
+        strcpy(name,"");
+        contor = 0;
+        token = strtok(Array_of_cross_roads[i],":");
+        strcat(name,token);
+        while(token != NULL){
+            token = strtok(NULL," ");
+        }
+        strcpy(Name_of_cross_roads[i],name);
+        printf("%s\n",Name_of_cross_roads[i]);
     }
 }
