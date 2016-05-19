8ef5367a991903ab6e6298c8cce9069f89aed779
vica86 <victoria.galanciuc@gmail.com>
2016-05-19 13:51:48 +0300

    Update super program v.0.0.4

--- SDADOOM.c
+++ SDADOOM.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 int main(){
     FILE* cross_road_file_pointer;
@@ -10,10 +11,14 @@ int main(){
     char Cross_Road[500]; //Array of char type which stores each line separately from the file
     char *token; //Pointer to char type, which is used as pointer to tokenized strings
     char name[200]; //Array of char which stores the name of each crossroad separately
-    int i = 0, j = 0, crossroad_initial, crossroad_final; //i,j used in for loops, crossroad_initial,crossroad_final used to store the number of crossroad where are person and where he wants to go
+    int i = 0, j = 0, contor = 0, crossroad_initial, crossroad_final; //i,j used in for loops, crossroad_initial,crossroad_final used to store the number of crossroad where are person and where he wants to go
     int number_of_crossroads = 3; //integer variable which stores the number of the crossroads in file
     int infinity = 1000000; //integer which holds a very big number because programs did not have infinity
-    int array[152] = {[0 ... 151] = infinity}; //Array of infinities, representing the distances from the selected point to every node in our graph
+    int Distances_of_chosen_crossroad[152] = {[0 ... 151] = infinity}; //Array of infinities, representing the distances from the selected point to every node in our graph
+    int Unvisited_array[152] = {[0 ... 151] = 0}; //Array of unvisited crossroads
+    int minimum,u; //Minimum variable,u variable to hold i;
+    int distance_changed; //Variable which holds the changed distance of a certain node;
+    time_t linear_start_time, linear_end_time; //Variables which holds the time of start and end of linear algorithm
 
     //Reading the file line by line and storing lines into Array_of_cross_roads
     while(!feof(cross_road_file_pointer)){
@@ -67,9 +72,40 @@ int main(){
 
     else{
 
-        //ITERATIVE PROCESSING
+        //LINEAR PROCESSING
+
+        linear_start_time = time(NULL);
+        Distances_of_chosen_crossroad[crossroad_initial-1] = 0;
+        while(contor != number_of_crossroads){
+            minimum = infinity;
+
+            for(i = 0; i < number_of_crossroads; i++){
+                if(Unvisited_array[i] == 0){
+                    if(Distances_of_chosen_crossroad[i] <= minimum){
+                        minimum = Distances_of_chosen_crossroad[i];
+                        u = i;
+                    }
+                }
+            }
+
+            for (i = 0; i < number_of_crossroads; i++){
+                if(Distances_between_cross_roads[u][i] != 0){
+                    distance_changed = Distances_of_chosen_crossroad[u] + Distances_between_cross_roads[u][i];
+                    if (distance_changed < Distances_of_chosen_crossroad[i]){
+                        Distances_of_chosen_crossroad[i] = distance_changed;
+                    }
+                }
+            }
+            Unvisited_array[u] = -1;
+            contor++;
+        }
+        linear_end_time = time(NULL);
+
+        printf("\nShortest distance %d\n",Distances_of_chosen_crossroad[crossroad_final-1]);
+        printf("Linear processing of the crossroads used %f, seconds \n", difftime(linear_end_time,linear_start_time));
 
 
         //PARALLEL PROCESSING
+
     }
 }
